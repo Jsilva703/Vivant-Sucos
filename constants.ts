c1a09f558df13942aa39c199c04248c105ca3eed
João Victor <jvs.devsilva@gmail.com>
2025-10-16 17:14:29 -0300

    Atualiza números de telefone e links de redes sociais nos constantes

--- constants.ts
+++ constants.ts
@@ -17,15 +17,15 @@ export const FLAVORS_IMAGE_URL =
   "https://mkdocs-201908.s3.sa-east-1.amazonaws.com/1624/production/b9f2cecc34e244a499d0a85f1cc1f10c_1624/production/Google_AI_Studio_2025-10-16T16_00_38_1760630885400.659Z";
 
 // IMPORTANT: Replace with your actual WhatsApp phone number
-export const WHATSAPP_PHONE_NUMBER = "5511999999999";
+export const WHATSAPP_PHONE_NUMBER = "5511911031972";
 
 // Fix: Export WHATSAPP_LINK, which was missing and caused an error in Commitment.tsx.
 export const WHATSAPP_LINK = `https://wa.me/${WHATSAPP_PHONE_NUMBER}`;
 
-export const INSTAGRAM_LINK = "https://www.instagram.com/"; // Placeholder
+export const INSTAGRAM_LINK = "https://www.instagram.com/jvs.devsilva/"; // Placeholder
 
 // IMPORTANT: Replace with your actual PIX key. It could be an email, phone, CPF/CNPJ or a random key.
-export const PIX_KEY = "a1b2c3d4-e5f6-g7h8-i9j0-k1l2m3n4o5p6"; // Placeholder for a random key
+export const PIX_KEY = "6d2fc34d-3113-457d-9f7f-f3fe550496a7"; // Placeholder for a random key
 export const PIX_KEY_TYPE = "Chave Aleatória"; // Example: 'E-mail', 'CNPJ', etc.
 export const MERCADO_PAGO_LINK = "https://link.mercadopago.com.br/devpages";
 
